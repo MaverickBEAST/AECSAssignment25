447c40c95701b25736e897fc2a58882d4d2375c1
MaverickBEAST <banuka.lahiru1111@gmail.com>
2025-04-16 00:38:20 +0530

    Services Endpoint URL Update V3

--- better-wellness-frontend/src/services/userService.js
+++ better-wellness-frontend/src/services/userService.js
@@ -1,7 +1,7 @@
 // src/services/userService.js
 import axios from "axios";
 
-const BASE_URL = "https://a8797e0c8948a4dd1b538090991eae26-1818452634.us-east-1.elb.amazonaws.com";
+const BASE_URL = "https://user.betterhealthservices.42web.io";
 
 const register = async (userData) => {
   const res = await axios.post(`${BASE_URL}/register`, userData);
