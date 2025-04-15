baf8a8f748a4cc2cdcf09430c01cb314d7081739
MaverickBEAST <banuka.lahiru1111@gmail.com>
2025-04-15 23:53:36 +0530

    Services Endpoint URL Update V2

--- better-wellness-frontend/src/services/counsellorService.js
+++ better-wellness-frontend/src/services/counsellorService.js
@@ -1,7 +1,7 @@
 // src/services/counsellorService.js
 import axios from "axios";
 
-const BASE_URL = "https://aeba3d810c60f47a3a7c707134d640f8-2037757430.us-east-1.elb.amazonaws.com";
+const BASE_URL = "aeba3d810c60f47a3a7c707134d640f8-2037757430.us-east-1.elb.amazonaws.com";
 
 const getCounsellors = async (token) => {
   const res = await axios.get(`${BASE_URL}/counsellors`, {
